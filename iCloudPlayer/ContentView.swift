531d7f2afd82fc71217e52c18ef48b25f770a8ce
shiima <shinpei8080@gmail.com>
2021-09-05 07:44:46 +0900

    Initial Commit

---
+++ iCloudPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iCloudPlayer
+//
+//  Created by Imada Shinpei on 2021/09/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iCloudPlayer/iCloudPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCloudPlayerApp.swift
+//  iCloudPlayer
+//
+//  Created by Imada Shinpei on 2021/09/05.
+//
+
+import SwiftUI
+
+@main
+struct iCloudPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
